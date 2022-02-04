f6dc77b99a733247f852dc3079a4cd4448a58fa6
bmstar <bmstar2012@gmail.com>
2022-02-04 22:17:10 +0800

    Update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -2,7 +2,7 @@ import { config } from 'dotenv';
 
 config();
 
-export const FLEX_END_DAYS = 84;
+export const FLEX_END_DAYS = 56;
 export const NETWORK_IS_TEST =
   process.env.ENVIRONMENT === 'dev' || process.env.ENVIRONMENT === 'devnet';
 export const SOLANA_NETWORK = NETWORK_IS_TEST ? 'devnet' : 'mainnet-beta';
